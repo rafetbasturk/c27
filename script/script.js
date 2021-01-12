2e47a6b3b9dc033ab154cc92c6029832c6b3c61e
rafetbasturk <62568554+rafetbasturk@users.noreply.github.com>
2021-01-12 21:24:16 +0300

    second commit

--- script/script.js
+++ script/script.js
@@ -5,6 +5,12 @@ $(function() {
         $("#close").css("display", "block");
         $("nav").fadeIn(300, function(){
             $(this).fadeIn();
+        }).css({
+            "position": "relative",
+            "z-index": "5"
+        });
+        $("main").css({
+            "background": "linear-gradient(180deg, rgba(0, 0, 0, 0.8) 10%, rgba(0, 0, 0, 0.5) 25%, transparent 100%)"
         });
     });
 
@@ -13,6 +19,9 @@ $(function() {
         $("#hamburger").css("display", "block");
         $("nav").fadeOut(300, function(){
             $(this).fadeOut();
+        });
+        $("main").css({
+            "background": "none"
         });
     });
 });
